5d8b35a426279a319a120e5801aac7a3466c92df
MrSalim86 <cph-ms1068@cphbusiness.dk>
2024-09-09 12:37:48 +0200

    OLA_1 update

---
+++ OLA_1/TCPClintApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPClintApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Connect to the server at the specified IP and port
+                TcpClient client = new TcpClient("4.tcp.eu.ngrok.io", 12215); // Replace SERVER_IP_ADDRESS with the actual server IP
+                Console.WriteLine("Connected to server");
+
+                // Get the network stream
+                NetworkStream stream = client.GetStream();
+
+                // Read message from the console
+                Console.Write("Enter message to send: ");
+                string message = Console.ReadLine();
+
+                // Send the message to the server
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Sent: " + message);
+
+                // Receive the mirrored message from the server
+                data = new byte[1024];
+                int bytesRead = stream.Read(data, 0, data.Length);
+                string responseData = Encoding.ASCII.GetString(data, 0, bytesRead);
+                Console.WriteLine("Received: " + responseData);
+
+                // Close everything
+                stream.Close();
+                client.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+    }
+}
---
+++ OLA_1/TCPServerApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a TCP/IP socket
+            TcpListener server = new TcpListener(IPAddress.Any, 12345);
+            server.Start();
+            Console.WriteLine("Server started, waiting for connection...");
+
+            while (true)
+            {
+                // Accept an incoming client connection
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Client connected.");
+
+                // Get the network stream
+                NetworkStream stream = client.GetStream();
+
+                // Buffer to store the incoming data
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                // Convert data bytes to string
+                string data = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Received: " + data);
+
+                // Mirror the message back to the client
+                byte[] msg = Encoding.ASCII.GetBytes(data);
+                stream.Write(msg, 0, msg.Length);
+                Console.WriteLine("Sent: " + data);
+
+                // Close the connection
+                client.Close();
+            }
+        }
+
+
+    }
+}
